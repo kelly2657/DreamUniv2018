3d3ddb06741f3338d4bc4999435f2c134a06248a
kelly2657 <38282240+kelly2657@users.noreply.github.com>
2020-06-17 18:51:27 +0900

    Add files via upload

---
+++ 기본문법.c
@@ -0,0 +1,36 @@
+/*
+	정수	 실수	문자
+	int		float	char	문자 1글자
+					string	문자열 2글자 이상을 의미
+					
+
+C언어 영역 {}
+파이썬 :
+
+C언어 코드 마침표 역할 ;(세미콜론)을 붙임.
+					 
+	
+	
+	main() :	프로그램 실행 영역을 담당
+	함수 앞에 있는 int : 함수 타입
+	함수 타입 두 종류
+	void : 결과값이 없음. 
+	int, float, char ... : 결과값 존재
+	 
+	return : 함수의 결과값 
+	
+	출력함수 
+	printf("출력할 데이터");
+*/
+#include <stdio.h>
+int main(){
+	
+	// 줄나누기 : \n
+	// 출력하는 문장 안에서 원하는 곳에 작성
+	 
+	printf("Hello\n");	// 자동 줄나눔이 없음 
+	printf("Hello");
+	
+	return 0;	// 0  off를 의미 -> 프로그램 종료 
+}
+
